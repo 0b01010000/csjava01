4e948df4fc0954b1f617327890b76573f774d3bf
Taffarel Oliveira <202103371094@alunos.estacio.br>
2022-10-21 01:32:17 +0000

    add example: PrintTest from echua online class

---
+++ examples/PrintTest.java
@@ -0,0 +1,14 @@
+package examples;
+
+public class PrintTest {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.print("Hello, World again!");
+        System.out.println("Hello");
+        System.out.println();
+
+        System.out.println("Hello, World!");
+        System.out.print("Hello, World again again!");
+        System.out.println("Hello");
+    }
+}
