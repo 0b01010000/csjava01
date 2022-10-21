11a98bfcd5024acfa22e8ad1ee640bed4778217c
Taffarel Oliveira <202103371094@alunos.estacio.br>
2022-10-21 02:01:26 +0000

    add exercises: PrintChecker... from ehchua's class

---
+++ exercises/PrintSheepPattern.java
@@ -0,0 +1,22 @@
+package exercises;
+
+public class PrintSheepPattern {
+    public static void main(String[] args) {
+         /*                     '__'
+                                (oo)
+                        /========//
+                       / || @@ ||
+                      o  ||----||
+                         VV    VV
+                         ''    ''
+        */
+        System.out.println("          '__'");                    
+        System.out.println("          (oo)");
+        System.out.println("  /========// ");
+        System.out.println(" / || @@ ||   ");
+        System.out.println("*  ||----||   ");
+        System.out.println("   VV    VV   ");
+        System.out.println("   ''    ''");
+                    
+    }
+}
